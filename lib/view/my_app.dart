37cb51a646c1ed1f1c2a58c01242141cd6aee0fe
Gabriel Souza <gabriel12saraiva@gmail.com>
2022-05-27 15:42:27 -0300

    creates the basic folder structure

---
+++ lib/view/my_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:formulario_de_atendimento/view/screens/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
