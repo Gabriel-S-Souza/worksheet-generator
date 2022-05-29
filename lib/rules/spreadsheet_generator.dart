b49b2a02c0559bc0add1d3727d26449c78e8b3f5
Gabriel Souza <gabriel12saraiva@gmail.com>
2022-05-29 12:26:59 -0300

    creates the Spreadsheet Generator class

---
+++ lib/rules/spreadsheet_generator.dart
@@ -0,0 +1,99 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
+import 'package:excel/excel.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show ByteData, PlatformException, rootBundle;
+import 'package:permission_handler/permission_handler.dart';
+
+class SpreadsheetGenerator {
+  final String spredsheetPath;
+  final String spreadsheetName;
+  SpreadsheetGenerator({
+    required this.spredsheetPath,
+    required this.spreadsheetName
+  }) {
+    _initDownloadsDirectoryState();
+    _getSpreadsheet(spredsheetPath);
+  }
+  
+  late final Excel _excel;
+  late final Directory _downloadsDirectory;
+
+  //TODO: needs to be tested
+  /// Updates the cell
+  void updateCell(
+    String sheetName,
+    CellIndex cellIndex,
+    dynamic value, {
+    CellStyle? cellStyle,
+    }) {
+      _excel.updateCell(
+        sheetName,
+        cellIndex,
+        value,
+        cellStyle: cellStyle,
+      );
+  }
+
+  // TODO: needs to be implemented
+  Future<void> sendByEmail() async {}
+
+
+  //TODO: needs to be tested
+  /// Save the spreadsheet to the downloads directory and return the path.
+  Future<String> exportFile() async {
+    final List<int> bytes = _excel.encode()!;
+
+    Uint8List data = Uint8List.fromList(bytes);
+
+    await _writeFile(data, '${spreadsheetName}_${UniqueKey().toString().substring(2, 7)}.xlsx');
+
+    if (_downloadsDirectory != null) {
+      return _downloadsDirectory.path;
+    } else {
+      return Exception('Cannot find download directory').toString();
+    }
+  }
+
+  //TODO: needs to be tested
+  /// Write the file to the downloads directory.
+  Future<File> _writeFile(Uint8List data, String name) async {
+
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      await Permission.storage.request();
+    }
+
+    Directory tempDir = _downloadsDirectory;
+    String tempPath = tempDir.path;
+    var filePath = '$tempPath/$name';
+
+    var bytes = ByteData.view(data.buffer);
+    final buffer = bytes.buffer;
+
+    return File(filePath).writeAsBytes(buffer.asUint8List(data.offsetInBytes, data.lengthInBytes));
+  }
+
+  //TODO: needs to be tested
+  /// Get the downloads directory.
+  Future<void> _initDownloadsDirectoryState() async {
+    Directory? downloadsDirectory;
+    try {
+      downloadsDirectory = await DownloadsPathProvider.downloadsDirectory;
+    } on PlatformException {
+      throw Exception('Could not get the downloads directory');
+    }
+    if(downloadsDirectory != null) {
+      _downloadsDirectory = downloadsDirectory;
+    }
+  }
+  
+  /// Get the spreadsheet from the path.
+  Future<void> _getSpreadsheet(String path) async {
+    ByteData data = await rootBundle.load(path);
+    var bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+    _excel = Excel.decodeBytes(bytes);
+  }
+}
